f9112953fc4c1de1308bb80d87204d1591be192e
sudosurootdev <sudosurootdev@gmail.com>
2014-06-06 14:06:42 -0400

    Update version for otaupdater

--- Private.mk
+++ Private.mk
@@ -165,7 +165,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # More of my build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=sudosurootdev \
-    otaupdater.otaver=4.4.2 \
+    otaupdater.otaver=4.4.3 \
     otaupdater.otatime=$(shell date +"%Y%M%d-%H%M")
 
 # init.d Tweaks
