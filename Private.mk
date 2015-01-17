5743546d491772467d42a914d6395fb2ab4443c8
sudosurootdev <sudosurootdev@gmail.com>
2015-01-16 20:28:36 -0500

    Seperate sqlite & fix_permissions scripts from being triggered together. Now removing /data/sqlite.log will only re-run sqlite script.
    ALSO: Remove exec perms & copying of 95fixperms into build until I either
    update it to be useful with Lollipop or to just get rid of it.

--- Private.mk
+++ Private.mk
@@ -77,6 +77,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # init.d Tweaks & xbin files
 PRODUCT_COPY_FILES += \
-    vendor/vanir-private/proprietary/etc/init.d/95VacnPerms:system/etc/init.d/95VacnPerms \
+    vendor/vanir-private/proprietary/etc/init.d/95sqlite:system/etc/init.d/95sqlite \
     vendor/vanir-private/proprietary/xbin/freex:/system/xbin/freex \
     vendor/vanir-private/proprietary/xbin/calibrate-battery:/system/xbin/calibrate-battery
