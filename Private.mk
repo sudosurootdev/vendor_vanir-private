1a76cbe4297c070e175d9a39d584e1d6a0560d73
sudosurootdev <sudosurootdev@gmail.com>
2015-01-20 06:43:34 -0500

    Revert "[TEMP] Remove some props until all issues are worked out."
    This reverts commit e79d99d337b8b19ed8ffcb316045265a475e09c6.

--- Private.mk
+++ Private.mk
@@ -7,19 +7,50 @@
 
 RESERVED=""
 
+ifneq ($(filter $(TARGET_DEVICE),d800 d801 d802 d803 vs980 ls980),)
+
 # My build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.service.adb.enable=1 \
-    persist.sys.usb.config=adb \
-    persist.sys.root_access=3 \
-    service.adb.root=1 \
+    ro.ril.hsxpa=5 \
+    ro.ril.gprsclass=34 \
+    ro.ril.hep=1 \
+    ro.ril.hsdpa.category=28 \
+    ro.ril.enable.3g.prefix=1 \
+    ro.ril.htcmaskw1.bitmask=4294967295 \
+    ro.ril.htcmaskw1=268449905 \
+    ro.ril.hsupa.category=9 \
+    ro.ril.def.agps.mode=1 \
+    ro.ril.def.agps.feature=2 \
+    ro.ril.enable.sdr=1 \
+    ro.ril.enable.gea3=1 \
+    ro.ril.enable.fd.plmn.prefix=23402,23410,23411 \
+    ro.ril.enable.a52=1 \
+    ro.ril.enable.a53=1 \
+    ro.ril.enable.dtm=1 \
+    net.tcp.buffersize.default=4096,87380,110208,4096,16384,110208 \
+    net.tcp.buffersize.wifi=262144,524288,1048576,262144,524288,1048576 \
+    net.tcp.buffersize.lte=262144,524288,3145728,262144,524288,3145728 \
+    net.tcp.buffersize.evdo_b=6144,262144,1048576,6144,262144,1048576 \
+    net.tcp.buffersize.umts=4096,87380,110208,4096,16384,110208 \
+    net.tcp.buffersize.hsdpa=6144,262144,1048576,6144,262144,1048576 \
+    net.tcp.buffersize.hspa=4096,87380,262144,4096,16384,262144 \
+    net.tcp.buffersize.hsupa=4094,87380,110208,4096,16384,110208 \
+    net.tcp.buffersize.gprs=6144,8760,11680,4096,8760,11680 \
+    net.tcp.buffersize.edge=6144,26280,35040,4096,16384,35040 \
+    ro.min_pointer_dur=8 \
     ro.secure=0 \
-    ro.debuggable=1 \
     ro.adb.secure=0 \
+    ro.debuggable=1 \
+    ro.allow.mock.locations=1 \
+    ro.allow.mock.sms=1 \
+    persist.service.adb.enable=1 \
+    persist.sys.root_access=3 \
+    persist.sys.usb.config=adb \
+    service.adb.root=1 \
     pm.sleep_mode=0
 
 # WiFi Reserved Properties
-# PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     net.dns1=8.8.8.8 \
     net.dns2=8.8.4.4 \
     net.ppp0.dns1=$(RESERVED) \
@@ -65,6 +96,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.wlan0.dns1=$(RESERVED) \
     net.wlan0.dns2=$(RESERVED) \
     wlan.driver.status=$(RESERVED)
+
+else
+
+# Common build.prop overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.secure=0 \
+    ro.debuggable=1 \
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=adb \
+    persist.sys.root_access=3 \
+    service.adb.root=1 \
+    ro.adb.secure=0 \
+    net.dns1=8.8.8.8 \
+    net.dns2=8.8.4.4 \
+    pm.sleep_mode=0
+
+endif
 
 # More of my build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
